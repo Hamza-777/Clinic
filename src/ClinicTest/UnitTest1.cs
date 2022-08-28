264e281f140dd16d64cbb3c3d9944cc37a404ab6
Hamza-777 <hamzararrani77@gmail.com>
2022-08-28 16:20:15 +0530

    Feat: Initiated a unit testing project

---
+++ src/ClinicTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using ClinicBack;
+
+namespace ClinicTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LoginTest()
+        {
+        }
+    }
+}
